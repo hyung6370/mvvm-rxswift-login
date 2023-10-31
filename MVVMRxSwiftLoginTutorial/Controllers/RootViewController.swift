055e5a706209b13c764d1d0d8842cb998a425c00
emptyhead <korea6370@yu.ac.kr>
2023-10-31 19:24:14 +0900

    ✨ [Add] : Google Auth 로그인 기능 추가

--- MVVMRxSwiftLoginTutorial/Controllers/RootViewController.swift
+++ MVVMRxSwiftLoginTutorial/Controllers/RootViewController.swift
@@ -8,8 +8,19 @@
 import UIKit
 import SnapKit
 import Then
+import RxSwift
+import RxCocoa
+import Firebase
+import GoogleSignIn
+import FirebaseAuth
 
 class RootViewController: UIViewController {
+    
+    let disposeBag = DisposeBag()
+    let viewModel = LoginViewModel()
+    
+    let userEmail = "test@test.com"
+    let userPassword = "test123"
     
     // Properties
     private let titleLabel = UILabel().then {
@@ -45,17 +56,43 @@ class RootViewController: UIViewController {
     private let loginButton = UIButton().then {
         $0.setTitle("Login", for: .normal)
         $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
-        $0.titleLabel?.textColor = .white
+        $0.setTitleColor(.white, for: .normal)
         $0.backgroundColor = #colorLiteral(red: 0.6, green: 0.8078431373, blue: 0.9803921569, alpha: 1)
+        $0.layer.cornerRadius = 15
+    }
+    
+    private let googleButton = UIButton().then {
+        $0.setTitle("Google로 로그인하기", for: .normal)
+        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = #colorLiteral(red: 0.0548138991, green: 0.6635760665, blue: 0.9648820758, alpha: 1)
+        $0.layer.cornerRadius = 15
+    }
+    
+    private let kakaoButton = UIButton().then {
+        $0.setTitle("Kakao로 로그인하기", for: .normal)
+        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        $0.setTitleColor(.black, for: .normal)
+        $0.backgroundColor = #colorLiteral(red: 0.9297463298, green: 0.921446979, blue: 0.1752431393, alpha: 1)
+        $0.layer.cornerRadius = 15
+    }
+    
+    private let appleButton = UIButton().then {
+        $0.setTitle("Apple로 로그인하기", for: .normal)
+        $0.titleLabel?.font = UIFont.boldSystemFont(ofSize: 15)
+        $0.setTitleColor(.white, for: .normal)
+        $0.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         $0.layer.cornerRadius = 15
     }
     
     // Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
         view.backgroundColor = .white
+        
+        setupControl()
         configureUI()
+        
     }
 
     // Helpers
@@ -101,7 +138,103 @@ class RootViewController: UIViewController {
             $0.top.equalTo(passwordView.snp.bottom).offset(50)
             $0.leading.trailing.equalTo(emailView)
         }
+        
+        view.addSubview(googleButton)
+        googleButton.snp.makeConstraints {
+            $0.top.equalTo(loginButton.snp.bottom).offset(10)
+            $0.leading.trailing.equalTo(emailView)
+        }
+        
+        view.addSubview(kakaoButton)
+        kakaoButton.snp.makeConstraints {
+            $0.top.equalTo(googleButton.snp.bottom).offset(10)
+            $0.leading.trailing.equalTo(emailView)
+        }
+        
+        view.addSubview(appleButton)
+        appleButton.snp.makeConstraints {
+            $0.top.equalTo(kakaoButton.snp.bottom).offset(10)
+            $0.leading.trailing.equalTo(emailView)
+        }
     }
-
     
+    func setupControl() {
+        emailTextField.rx.text
+            .orEmpty
+            .bind(to: viewModel.emailObserver)
+            .disposed(by: disposeBag)
+        
+        passwordTextField.rx.text
+            .orEmpty
+            .bind(to: viewModel.passwordObserver)
+            .disposed(by: disposeBag)
+        
+        viewModel.isValid.bind(to: loginButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        
+        viewModel.isValid
+            .map { $0 ? 1 : 0.3 }
+            .bind(to: loginButton.rx.alpha)
+            .disposed(by: disposeBag)
+        
+        loginButton.rx.tap.subscribe (
+            onNext: { [weak self] _ in
+                if self?.userEmail == self?.viewModel.emailObserver.value &&
+                    self?.userPassword == self?.viewModel.passwordObserver.value {
+                    let alert = UIAlertController(title: "로그인 성공", message: "환영합니다.", preferredStyle: .alert)
+                    let ok = UIAlertAction(title: "확인", style: .default) { _ in
+                        let homeVC = HomeViewController()
+                        self?.navigationController?.pushViewController(homeVC, animated: true)
+                    }
+                    alert.addAction(ok)
+                    self?.present(alert, animated: true, completion: nil)
+                }
+                else {
+                    let alert = UIAlertController(title: "로그인 실패", message: "아이디 혹은 비밀번호를 다시 확인해주세요.", preferredStyle: .alert)
+                    let ok = UIAlertAction(title: "확인", style: .default)
+                    alert.addAction(ok)
+                    self?.present(alert, animated: true, completion: nil)
+                }
+            }
+        ).disposed(by: disposeBag)
+        
+        googleButton.rx.tap.subscribe(
+            onNext: { [weak self] in
+                self?.handleGoogleLogin()
+            }).disposed(by: disposeBag)
+        
+        
+        
+        
+        // MARK: - Google Login
+        
+        
+        
+        // MARK: - Kakao Login
+        
+        
+        // MARK: - Apple Login
+        
+    }
+    
+    private func handleGoogleLogin() {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { [unowned self] result, error in
+            guard error == nil else {
+                return
+            }
+            
+            guard let user = result?.user, let idToken = user.idToken?.tokenString else { return }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+            
+            Auth.auth().signIn(with: credential) { _,_ in
+                self.dismiss(animated: true)
+            }
+        }
+    }
 }
--- MVVMRxSwiftLoginTutorial/ViewModels/LoginViewModel.swift
+++ MVVMRxSwiftLoginTutorial/ViewModels/LoginViewModel.swift
@@ -5,4 +5,18 @@
 //  Created by KIM Hyung Jun on 10/31/23.
 //
 
-import Foundation
+import RxSwift
+import RxRelay
+
+class LoginViewModel {
+    let emailObserver = BehaviorRelay<String>(value: "")
+    let passwordObserver = BehaviorRelay<String>(value: "")
+    
+    var isValid: Observable<Bool> {
+        return Observable.combineLatest(emailObserver, passwordObserver)
+            .map { email, password in
+                print("Email: \(email), Password: \(password)")
+                return !email.isEmpty && email.contains("@") && email.contains(".") && password.count > 0
+            }
+    }
+}
